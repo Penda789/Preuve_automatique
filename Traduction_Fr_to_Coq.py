60fdc23754ed5b53a0f6e4fc39cdb8a9c352492e
Penda SOW <161828291+Penda789@users.noreply.github.com>
2026-03-31 13:34:24 +0200

    Add files via upload
    La traduction des ennoncé fonctionne desormais ( voir métrique dans le benchmark que je vais faire et en fonction des resultat, amelioré la traduction des tactiques COQ grace à des AST ? ). Maintenant, j'essaye de les executer automatiquement.

--- Traduction_Fr_to_Coq.py
+++ Traduction_Fr_to_Coq.py
@@ -4,11 +4,15 @@
 import subprocess
 import os
 from mistralai.client import Mistral
+import re
 
-def trad_enonce(texte_fr, max_tentatives=3):
-    
+def trad_enonce():
+    #cle api "0Lrp5o1vP2xjfXv7hh7s5zkAdd3PWNxM"
+    #pTekYKZfndlGAm2mHt8USDw5rh50YBei
 
     tentative = 0
+    max_tentatives=4
+    texte_fr=input("Entrez l'ennoncé à prouver:")
 
     key = os.getenv("MISTRAL_API_KEY")  
     if not key:
@@ -23,10 +27,21 @@ def trad_enonce(texte_fr, max_tentatives=3):
             response = mistral.chat.complete(
                 model="mistral-small-latest",
                 messages=[
-                    {"role": "user", "content": f"Traduis cet énoncé en Coq avec preuve automatique: {texte_fr}"}
+                    {"role": "user", "content": f"Traduis cet énoncé en langage Coq : {texte_fr}, ne me donne pas d'indication. Contente toi uniquement de produire en coq"}
                 ]
             )
-            code_coq = response.choices[0].message.content
+
+            code_coq=response.choices[0].message.content
+            # Vérification que la réponse n'est pas vide
+            if not code_coq:
+                erreur = "Réponse vide de l'API Mistral"
+                texte_fr = f"Erreur: {erreur}\nRéessaie de traduire: {texte_fr}"
+                continue  # passe à la tentative suivante
+
+            """#pour qu'il y ai que du COQ sinon met la rep de mistral aussi dedans et dcp compilation echoue
+            match = re.search(r"```coq\n(.*?)```", code_coq, re.DOTALL)
+            if match:
+                code_coq = match.group(1)"""
             
             # Écriture fichier temp
             with open("temp.v", "w") as f:
@@ -41,9 +56,9 @@ def trad_enonce(texte_fr, max_tentatives=3):
                 erreur = result.stderr
                 texte_fr = f"Erreur Coq: {erreur}\nCorrige ce code Coq: {code_coq}"
         
-    return "Échec après {max_tentatives} tentatives"
+    return f"Échec après {max_tentatives} tentatives"
 
 if __name__ == "__main__":
     enonce = "Prouve que pour tout n nombre naturel, n + 0 = n."
-    print(trad_enonce(enonce))
+    trad_enonce()
 
